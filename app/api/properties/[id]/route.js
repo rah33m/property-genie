8b5434148a2d32228e065164498057deb92674c4
rah33m <38769981+rah33m@users.noreply.github.com>
2024-06-14 18:33:41 +0800

    bug fix and edit property section

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -10,9 +10,7 @@ export const GET = async (request, { params }) => {
 
     if (!property) return new Response("Property not found", { status: 404 });
 
-    return new Response(JSON.stringify({ property }), {
-      status: 200,
-    });
+    return Response.json(property);
   } catch (error) {
     console.log(error);
     return new Response("Something went wrong", { status: 500 });
